5a76476961e8f6d8fa9f35c1941e8dd84ab2c6d3
rbklp7828 <rbklp7828@gmail.com>
2026-02-19 00:27:39 +0800

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'baccarat-v1';
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./'])));
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
