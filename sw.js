7994151aa8834b7a86b979543fd81d05c0d8bd67
rbklp7828 <rbklp7828@gmail.com>
2026-02-19 14:52:02 +0800

    Upgrade service worker to version 2
    Updated service worker to version 2 and modified caching strategy.

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
-const CACHE = 'baccarat-v1';
+const CACHE = 'baccarat-v2';   // 已升級到 v2，強制更新
+
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./'])));
+    e.waitUntil(
+        caches.open(CACHE).then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json'
+            ]);
+        })
+    );
 });
+
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
 });
